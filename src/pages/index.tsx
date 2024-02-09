95ead00f16c98c60777e95705445801ed721bc1c
Saleh Alikhani <salehalikhani@gmail.com>
2024-02-09 21:47:57 +0330

    husky setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ import styles from '@/styles/Home.module.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  console.log('test');
   return (
     <>
       <Head>
